3e4eed5aec25a4d4e6a2bc963735245b352c9d5a
rot <appl.rot13@gmail.com>
2023-12-16 11:25:19 +0900

    Add IP address and MAC address as constants

--- MagicPacket.bat
+++ MagicPacket.bat
@@ -1,2 +1,12 @@
 @echo off
-powershell "$mac=[byte[]]("""c8:60:00:dd:ea:b9""".split(""":""") | ForEach-Object{[Convert]::ToInt32($_, 16)}); $message=[byte[]](@(0xff) * 6) + $mac * 16; $udpClient=new-object System.Net.Sockets.UdpClient; $udpClient.Connect([System.Net.IPAddress]::Broadcast, 3708); $udpClient.Send($message, $message.Length) | out-null; $udpClient.Close();"
+
+set IP_ADDRESS=192.168.64.15
+set MAC_ADDRESS=c8:60:00:dd:ea:b9
+
+if "%IP_ADDRESS%"=="" (
+	set PARSE_CODE=Broadcast
+) else (
+	set PARSE_CODE=Parse^("""%IP_ADDRESS%"""^)
+)
+
+powershell "$mac=[byte[]]("""%MAC_ADDRESS%""".split(""":""") | ForEach-Object{[Convert]::ToInt32($_, 16)}); $message=[byte[]](@(0xff) * 6) + $mac * 16; $udpClient=new-object System.Net.Sockets.UdpClient; $udpClient.Connect([System.Net.IPAddress]::%PARSE_CODE%, 3708); $udpClient.Send($message, $message.Length) | out-null; $udpClient.Close();"
